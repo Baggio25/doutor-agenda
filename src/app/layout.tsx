b8721956eeae87401dd36dd62ab073c1d17e6a70
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-05-29 22:44:13 -0300

    feat: add login

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
+import "./globals.css";
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +32,7 @@ export default function RootLayout({
         cz-shortcut-listen="true"
       >
         {children}
+        <Toaster position="bottom-center" richColors theme="light" />
       </body>
     </html>
   );
