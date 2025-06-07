cedc24780b9e1d9a5b73f5aa403b4556c88529cf
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-06-07 13:35:55 -0300

    feat: ensure date is available on appointment creation

--- src/app/(protected)/appointments/_components/upsert-appointment-form.tsx
+++ src/app/(protected)/appointments/_components/upsert-appointment-form.tsx
@@ -104,6 +104,7 @@ const UpsertAppointmentForm = ({
         date: dayjs(selectedDate).format("YYYY-MM-DD"),
         doctorId: selectedDoctorId,
       }),
+    enabled: !!selectedDate && !!selectedDoctorId,
   });
 
   // Atualizar o preço quando o médico for selecionado
